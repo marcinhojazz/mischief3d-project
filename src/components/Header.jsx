e63c31ee0bae5194f800fb6ad1646a89d1720704
marcinhojazz <marcinhojazz@gmail.com>
2022-12-12 22:47:23 -0300

    bug fix text size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className="absolute pointer-events-none z-20 w-full h-full bg-transparent flex flex-col justify-between items-center">
-      <h1 className='font-extrabold text-cyan-100 text-7xl py-12 color'>MISCHIEF3D</h1>
+      <h1 className='font-extrabold text-cyan-100 text-5xl lg:text-7xl  py-12 color'>MISCHIEF3D</h1>
     </header >
   )
 }
